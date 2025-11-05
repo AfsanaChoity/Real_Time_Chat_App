36ab877488cf62aad1c1de8be0b39d4b2a847086
choity <afsanamimi194@gmail.com>
2025-11-05 18:36:23 +0600

    set-up backend

---
+++ Server/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import "dotenv/config";
+import cors from 'cors';
+import http from 'http';
+import { connectDB } from './lib/db.js';
+import userRouter from './routes/userRoutes.js';
+
+
+// Create an Express application and HTTP server
+
+const  app = express();
+const server = http.createServer(app);
+
+// Middleware
+app.use(express.json({limit: "10mb"}));
+app.use(cors());
+
+// routes setup
+app.use("/api/status", (req, res)=> res.send("Server is Live"))
+app.use("/api/auth", userRouter);
+
+// Connect to MongoDB
+await connectDB()
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server is running on port : ${PORT}`));
